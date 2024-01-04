d0ed6b13e4d2898c7a0d63ce34155c908a1fe295
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-04 03:14:44 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //Treates all js code as newer version
+
+//alert(3+3)=> dont run-> we are using nodejs not browser.
+
+//DATATYPES//
+
+let name = "Sameer"
+let age = 24
+let isSucceeded = false
+let state;
+
+
+//PRIMITIVE DATATYPE
+//number ->Range = 2 to power 53
+//bigint
+//string 
+//boolean
+//NULL -> standalone value
+//undefined ->empty value
+//symbol ->unique
+
+//Object
+
+console.log(typeof name);       //o/p->string
+console.log(typeof null)        //o/p->object
+console.log(typeof undefined)   //o/p->undefined
