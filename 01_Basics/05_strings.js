56ba449a42ee04435ef15f21dd9ad7f735674e6e
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-07 02:22:50 +0000

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,35 +1,40 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value");-->bad way
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
-
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+const gameName = new String('hitesh-hc-com')//->another way of initializing string
+//string is an object, have its own method.
 
 
-// console.log(gameName.length);
+// console.log(gameName[0]);->op=h
+// console.log(gameName.__proto__);->op={}
+
+
+// console.log(gameName.length);->op=13
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.charAt(2));//->op=t
+console.log(gameName.indexOf('t'));//->op=2
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+const newString = gameName.substring(0, 4)//doesnt consider negativer value if given treat them as 0
+console.log(newString);//->op=hite
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+const game = new String('hitesh-hc')
+const anotherString = game.slice(-8, 4)//can give negative value->cpunt start from back 
+console.log(anotherString);//->op=ite
 
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne);//op->   hitesh    
+console.log(newStringOne.trim());//op->hitesh-->removes end and start spaces
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-'))//op-<https://hitesh.com/hitesh-choudhary-->%20 is replaced by -
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))//op->false-->sundar is not present in url string
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));//convert string into array on the basis of - 
+//op-> ['hitesh', 'hc' ,'com']
+//we can also done it on the basis of spaces we have onlye to use ' ' in place of '-'
