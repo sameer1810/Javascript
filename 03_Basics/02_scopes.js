acefaf41da2f8220fe2fe5a6f84c1c30ece563a4
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-11 11:50:57 +0000

    scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -15,4 +15,48 @@ if (true) {
 // console.log(c);-->c=30-->var doesnt have any scope
 
 
+/*+++  NESTED SCOPES  +++*/
 
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);//runs fine
+    }
+    // console.log(website);-->Eroor , out of scope
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);->runs fine
+    }
+    // console.log(website);->Eroor , out of scope
+}
+
+// console.log(username);->Eroor , out of scope
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))//->THIS WONT GIVE ERROR
+//-<CAN USE BEFORE DECLARATION
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)//->THIS WILL GIVES ERROR 
+//-<CANT USE BEFORE DECLARATION,WHILE STORING IN VARIABLE
+const addTwo = function(num){
+    return num + 2
+}
