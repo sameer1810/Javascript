40857a903be01837f72e7e6e168b9280f1a46911
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-14 06:46:35 +0000

    for in loop

--- 05_iterations/04_for_in.js
+++ 05_iterations/04_for_in.js
@@ -1,3 +1,40 @@
 //for in loop
 //we can iterate over object through for in
 
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(key);//op->give all key
+    //console.log(myObject[key]);//op->give all value
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+//for in loop on arrays
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(key);//op->gives index
+    //console.log(programming[key]);//op->gives element
+}
+
+
+//for in loop on map
+//map is not iterable through for in loop
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
