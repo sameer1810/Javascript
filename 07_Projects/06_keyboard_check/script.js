4eb5eaa8c98a82a71a19d91be337d4beefe85ed9
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-24 10:40:56 +0000

    project

---
+++ 07_Projects/06_keyboard_check/script.js
@@ -0,0 +1,23 @@
+//print which key is pressed on keyboard
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {//event on windows
+  insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});
+
