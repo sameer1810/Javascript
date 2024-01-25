88a777fe13daf4f5c3d81426d14f344a154869e7
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-25 09:33:54 +0000

    hoisting

---
+++ 01_Basics/01_variables_hoisting.js
@@ -0,0 +1,55 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+HOISTING in js
+
+In JavaScript, a variable can be declared after it has been used.
+
+In other words; a variable can be used before it has been declared.
+
+x = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x;                     // Display x in the element
+
+var x; // Declare x
+
+*/
+/*
+
+JavaScript Initializations are Not Hoisted
+
+var x = 5; // Initialize x
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x + " " + y;           // Display x and y
+
+var y = 7; // Initialize y
+
+Does it make sense that y is undefined in the last example?
+
+This is because only the declaration (var y), not the initialization (=7) is hoisted to the top.
+
+Because of hoisting, y has been declared before it is used, but because initializations are not hoisted, the value of y is undefined.
+*/
