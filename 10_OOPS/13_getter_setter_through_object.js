9cba4446c49ed5b35164f5556a827be94781a0ea
sameer1810 <87411175+sameer1810@users.noreply.github.com>
2024-01-25 07:52:03 +0000

    getter setter through object

---
+++ 10_OOPS/13_getter_setter_through_object.js
@@ -0,0 +1,17 @@
+//getter setter through object
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)//this is a factory fn
+console.log(tea.email);
